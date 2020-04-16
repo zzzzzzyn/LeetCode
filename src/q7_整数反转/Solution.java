a97e85d23bdca6b83c2c12ffba061c3db1e0e382
xuyanan <15033351995@163.com>
2020-04-16 12:41:35 +0800

    q7.整数反转 q8.字符串转换整数

---
+++ src/q7_整数反转/Solution.java
@@ -0,0 +1,21 @@
+package q7_整数反转;
+
+/**
+ * Created by xyn on 2020/4/16
+ */
+public class Solution {
+
+    // 时间复杂度O(n)
+    public int reverse(int x) {
+        long temp = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            temp = temp * 10 + pop;
+
+            if (temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE) return 0;
+
+            x = x / 10;
+        }
+        return (int) temp;
+    }
+}
