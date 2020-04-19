5daae106e9523466185cc8ded2306514c7d1be0c
xuyanan <15033351995@163.com>
2020-04-19 10:04:00 +0800

    整数转罗马数字

---
+++ src/q12_整数转罗马数字/Solution.java
@@ -0,0 +1,23 @@
+package q12_整数转罗马数字;
+
+/**
+ * Created by xyn on 2020/4/19
+ */
+public class Solution {
+
+    // O(1)
+    public String intToRoman(int num) {
+        int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            while (num >= nums[i]) {
+                res.append(romans[i]);
+                num -= nums[i];
+            }
+        }
+
+        return res.toString();
+    }
+}
