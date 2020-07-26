4d2c27d094a3de8f772f7b65de209624eae1a64c
xyn <15033351995@163.com>
2020-07-26 12:49:05 +0800

    README

--- src/leetcode/q122_买卖股票的最佳时机II/Solution.java
+++ src/leetcode/q122_买卖股票的最佳时机II/Solution.java
@@ -1,4 +1,16 @@
 package leetcode.q122_买卖股票的最佳时机II;
 
 public class Solution {
+
+    // 上升阶段的总和
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2) return 0;
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                res += prices[i] - prices[i - 1];
+            }
+        }
+        return res;
+    }
 }
--- src/leetcode/q189_旋转数组/Solution.java
+++ src/leetcode/q189_旋转数组/Solution.java
@@ -1,4 +1,6 @@
 package leetcode.q189_旋转数组;
 
 public class Solution {
+
+
 }
