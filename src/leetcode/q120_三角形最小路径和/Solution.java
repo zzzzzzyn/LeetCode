060d6ee10c87bb64d4fac92a22b46826099467d5
xyn <15033351995@163.com>
2020-08-01 10:50:30 +0800

    leetcode： 120.三角形最小路径和

---
+++ src/leetcode/q120_三角形最小路径和/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.q120_三角形最小路径和;
+
+import java.util.List;
+
+public class Solution {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+    }
+}
--- src/leetcode/q300_最长上升子序列/Solution.java
+++ src/leetcode/q300_最长上升子序列/Solution.java
@@ -5,22 +5,25 @@ package leetcode.q300_最长上升子序列;
  */
 public class Solution {
 
-    // [10, 9, 2, 5, 3, 7, 101, 18]
+    // dp[i] = max(dp[j]+1 , dp[k]+1, dp[l]+1)
+    // nums[i]>nums[j]  nums[i]>nums[k]  nums[i]>nums[l]
     public int lengthOfLIS(int[] nums) {
         if (nums.length == 0) return 0;
         int[] dp = new int[nums.length];
-        dp[0] = 1;
         int maxres = 1;
-        for (int i = 1; i < dp.length; i++) {
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
             int maxval = 0;
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
-                    maxval = Math.max(dp[j], maxval);
+                    maxval = Math.max(maxval, dp[j]);
                 }
             }
             dp[i] = maxval + 1;
-            maxres = Math.max(dp[i], maxres);
+            maxres = Math.max(maxres, dp[i]);
         }
         return maxres;
     }
+
+
 }
