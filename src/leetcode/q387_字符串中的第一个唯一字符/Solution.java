028dca2d0de24629bc6ea116e6def04b9e665946
xyn <15033351995@163.com>
2020-08-02 12:06:06 +0800

    leetcode： 387.字符串中的第一个唯一字符

---
+++ src/leetcode/q387_字符串中的第一个唯一字符/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.q387_字符串中的第一个唯一字符;
+
+import java.util.HashMap;
+
+/**
+ * Created by xyn on 2020/8/2
+ */
+public class Solution {
+
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1)return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.firstUniqChar("dddccdbba"));
+    }
+}
