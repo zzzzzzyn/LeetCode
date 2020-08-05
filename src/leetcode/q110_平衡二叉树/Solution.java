fe4b17e78634935394cbe3869ec70ac7d48499ca
xyn <15033351995@163.com>
2020-08-05 22:59:19 +0800

    leetcode： 110.平衡二叉树

--- src/leetcode/q110_平衡二叉树/Solution.java
+++ src/leetcode/q110_平衡二叉树/Solution.java
@@ -2,15 +2,26 @@ package leetcode.q110_平衡二叉树;
 
 public class Solution {
 
-//    public boolean isBalanced(TreeNode root) {
-//        if (root == null) return true;
-//
-//
-//    }
-//
-//    public int maxDepth(TreeNode root) {
-//        if (root == null) return 0;
-//    }
+    private boolean flag = true;
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        maxDepth(root);
+        return flag;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftLen = 0, rightlen = 0;
+        if (root.left != null) {
+            leftLen = maxDepth(root.left) + 1;
+        }
+        if (root.right != null) {
+            rightlen = maxDepth(root.right) + 1;
+        }
+        if (Math.abs(rightlen - leftLen) > 1) flag = false;
+        return Math.max(leftLen, rightlen);
+    }
 
     class TreeNode {
         int val;
