af12abcbb3076ad74540b3bd909e273a437ee718
xuyanan <15033351995@163.com>
2020-08-12 13:31:40 +0800

    剑指offer 438.找到字符串中所有字母异位词

--- src/leetcode/q438_找到字符串中所有字母异位词/Solution.java
+++ src/leetcode/q438_找到字符串中所有字母异位词/Solution.java
@@ -1,9 +1,24 @@
 package leetcode.q438_找到字符串中所有字母异位词;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
 
-    //    public List<Integer> findAnagrams(String s, String p) {
-//
-//    }
+    public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> res = new ArrayList<>();
+        char[] pArray = p.toCharArray();
+        Arrays.sort(pArray);
 
+        for (int start = 0, end = p.length() - 1; end < s.length(); end++) {
+            char[] array = s.substring(start, end + 1).toCharArray();
+            Arrays.sort(array);
+            if (Arrays.equals(array, pArray)) {
+                res.add(start);
+            }
+            start++;
+        }
+        return res;
+    }
 }
--- src/leetcode/q498_对角线遍历/Solution.java
+++ src/leetcode/q498_对角线遍历/Solution.java
@@ -17,5 +17,6 @@ public class Solution {
             }
 
         }
+        return null;
     }
 }
