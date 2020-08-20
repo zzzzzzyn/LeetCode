c1fdffe08f8f1e7e4704e5e18d6a1f084ddc5301
xuyanan <15033351995@163.com>
2020-08-20 20:49:20 +0800

    leetcode 278.第一个错误版本

--- src/leetcode/q278_第一个错误版本/Solution.java
+++ src/leetcode/q278_第一个错误版本/Solution.java
@@ -2,9 +2,18 @@ package leetcode.q278_第一个错误版本;
 
 public class Solution {
 
-
     public int firstBadVersion(int n) {
-        
+        int left = 1, right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 
     /**
