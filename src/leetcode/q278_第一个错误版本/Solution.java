d6e0b28381dfe25d337d802c490e61d8fcfb0675
xuyanan <15033351995@163.com>
2020-08-20 20:20:58 +0800

    leetcode 278.第一个错误版本

---
+++ src/leetcode/q278_第一个错误版本/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.q278_第一个错误版本;
+
+public class Solution {
+
+
+    public int firstBadVersion(int n) {
+        
+    }
+
+    /**
+     * The isBadVersion API is defined in the parent class VersionControl.
+     */
+    private boolean isBadVersion(int version) {
+        return true;
+    }
+}
