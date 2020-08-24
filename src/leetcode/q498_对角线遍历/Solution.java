1a58934c362c1c495ec0017ab1e59c86ee7a5474
xuyanan <15033351995@163.com>
2020-08-24 20:23:38 +0800

    面试题 1.8.零矩阵

--- src/leetcode/q498_对角线遍历/Solution.java
+++ src/leetcode/q498_对角线遍历/Solution.java
@@ -1,22 +1,66 @@
 package leetcode.q498_对角线遍历;
 
+import java.util.ArrayList;
+
 public class Solution {
 
     public int[] findDiagonalOrder(int[][] matrix) {
-
-        int[] res = new int[matrix.length * matrix[0].length];
-        // 方向变量
+        int i = 0, j = 0, l = 0;
         boolean goingDown = false;
-        int l = 0, r = 0, i = 0;
-        while (l < matrix.length && r < matrix[0].length) {
-            res[i] = matrix[l][r];
+        int[] res = new int[matrix.length * matrix[0].length];
+
+        while (i < matrix.length && j < matrix[0].length) {
+            res[l] = matrix[i][j];
             if (goingDown) {
-                l++;
+                i++;
+                if (j != 0) j--;
             } else {
-                r++;
+                if (i != 0) i--;
+                j++;
+            }
+            if (i == 0 || j == 0) goingDown = !goingDown;
+        }
+
+        return res;
+    }
+
+    public int[] findDiagonalOrder2(int[][] matrix) {
+        int index = 0;
+        int[] res = new int[matrix.length * matrix[0].length];
+
+        for (int k = 0; k < matrix[0].length; k++) {
+
+            int i = 0, j = k;
+            ArrayList<Integer> list = new ArrayList<>();
+            while (i < matrix.length && j >= 0) {
+                list.add(matrix[i][j]);
+                i++;
+                j--;
             }
 
+            if (k % 2 == 0) {
+                // 倒序
+                for (int m = list.size() - 1; m >= 0; m--) {
+                    res[index++]=list.get(m);
+                }
+            }else {
+                // 正序
+                for (int m = 0; m < list.size(); m++) {
+                    res[index++]=list.get(m);
+                }
+            }
+            list.clear();
         }
-        return null;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        for (int i : new Solution().findDiagonalOrder2(new int[][]{
+                new int[]{1, 2, 3},
+                new int[]{4, 5, 6},
+                new int[]{7, 8, 9}
+        })) {
+            System.out.print(i + ",");
+        }
     }
 }
