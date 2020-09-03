85611dabff1ed70c27cb4a1c67cdec537079a807
xuyanan <15033351995@163.com>
2020-09-03 09:59:02 +0800

    leetcode 268.缺失数字

--- src/leetcode/q268_缺失数字/Solution.java
+++ src/leetcode/q268_缺失数字/Solution.java
@@ -1,5 +1,7 @@
 package leetcode.q268_缺失数字;
 
+import java.util.Arrays;
+
 public class Solution {
 
     // ^ 运算(1^1=0,0^1=1)
@@ -10,5 +12,15 @@ public class Solution {
             mask++;
         }
         return res ^ mask;
+    }
+
+    public int missingNumber2(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (res != nums[i]) break;
+            res++;
+        }
+        return res;
     }
 }
