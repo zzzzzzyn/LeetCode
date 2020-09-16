e1e11a9c3b109854af005ad90bc5b6892d70d0c4
xuyanan <15033351995@163.com>
2020-09-16 13:49:10 +0800

    leetcode 112.路径总和

---
+++ src/leetcode/q112_路径总和/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.q112_路径总和;
+
+public class Solution {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return sum == root.val;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
